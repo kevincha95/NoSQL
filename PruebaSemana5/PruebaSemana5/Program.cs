fc7cc47ae93aa3549e27d7a9283f53a30b5ab74f
KEVIN CHAMBERLAIN <kchamberlain70853@ufide.ac.cr>
2020-06-26 20:16:37 -0600

    Hola mundo. Kevin Chamberlain

---
+++ PruebaSemana5/PruebaSemana5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaSemana5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            iniciar();
+        }
+
+        static void iniciar()
+        {
+            Console.WriteLine("HOLA MUNDO");
+            Console.ReadLine();
+        }
+    }
+}
